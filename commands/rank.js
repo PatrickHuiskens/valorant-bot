50a51a608345fd05f4bec707a0d6aaf880633c54
SvenTimmers@PC <sventimmerss@gmail.com>
2021-04-15 12:53:01 +0200

    Added leaderboardrank

--- commands/rank.js
+++ commands/rank.js
@@ -1,5 +1,6 @@
 const Bot = require('../bot.js');
 const Discord = require('discord.js');
+const request = require('request');
 const Tiers = require('../model/tiers.js');
 
 module.exports = {
@@ -26,7 +27,7 @@ function getPlayerMMR(playerId, msg) {
 
         response.data.Matches.every(element => {
             if(element.TierAfterUpdate != 0) {
-                sendRank(element, msg);
+                sendRank(element, msg, playerId);
                 rankFound = true;
                 return false;
             }
@@ -42,21 +43,52 @@ function getPlayerMMR(playerId, msg) {
     });
 }
 
-function sendRank(element, msg) {        
+function sendRank(element, msg, playerId) {        
     let rankEmbed = new Discord.MessageEmbed()
         .addField('Current Tier:', `${Tiers[element.TierAfterUpdate]}`);
     
-    if(element.TierAfterUpdate >= 21){
-        rankEmbed.addField('Current Rating: ', `${element.RankedRatingAfterUpdate}`)
-    } else {
-        rankEmbed.addField('Current Tier Progress: ', `${element.RankedRatingAfterUpdate}/100`)
-    }
-
     if(element.TierAfterUpdate > 2) {
         let attachment = new Discord.MessageAttachment(`./resources/ranks/${element.TierAfterUpdate}.png`, 'rank.png');
         rankEmbed.attachFiles(attachment)
                  .setThumbnail('attachment://rank.png');
     }
 
-    msg.channel.send(rankEmbed);
+    if(element.TierAfterUpdate >= 21){
+        getLeaderboardRank(playerId).then(function(leaderboardRank) {
+            console.log(leaderboardRank);
+            rankEmbed.addField('Current Leaderboard Rank: ', `${leaderboardRank}`);
+            rankEmbed.addField('Current Rating: ', `${element.RankedRatingAfterUpdate}`);
+            msg.channel.send(rankEmbed);
+        }).catch(function(err) {
+            rankEmbed.addField('Current Leaderboard Rank: RANK_NOT_FOUND');
+            rankEmbed.addField('Current Rating: ', `${element.RankedRatingAfterUpdate}`);
+            msg.channel.send(rankEmbed);
+        });
+    } else {
+        rankEmbed.addField('Current Tier Progress: ', `${element.RankedRatingAfterUpdate}/100`);
+        msg.channel.send(rankEmbed);
+    }
+}
+
+function getLeaderboardRank(playerid) {
+    return new Promise(function(resolve, reject){
+        request('https://api.henrikdev.xyz/valorant/v1/leaderboard/eu', function (error, response, body) {
+            if (response.statusCode === 200) {
+                let data = JSON.parse(body).data;
+                let leaderboardRank;
+                
+                data.every(player => {
+                    if(player.puuid == playerid) {
+                        leaderboardRank = player.leaderboardRank;
+                        return false;
+                    }
+                    return true;
+                });
+
+                resolve(leaderboardRank);
+            } else {
+                reject(error);
+            }
+        });
+    });
 }
